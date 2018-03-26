80c84dbac2d829a5090d18ee1244dd3328d461d5
Carly Webb <carlywebb@gmail.com>
2018-03-26 13:39:36 -0700

    player bar correct

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,28 +1,30 @@
-{
-  $('button#play-pause').on('click', function() {
+
+
+  $( document ).ready(function() {
+  $('button#play-pause').click( function() {
     player.playPause();
     $(this).attr('playState', player.playState);
   });
 
-  $('button#next').on('click', function() {
-    if (player.playState !== 'playing') { return; }
-
   $('button#previous').on('click', function() {
     if (player.playState !== 'playing') { return; }
-    });
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex + 1;
-    if (nextSongIndex >= album.songs.length) { return; }
+    const prevSongIndex = currentSongIndex - 1;
+    if (currentSongIndex < 1) { return; }
 
-    const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
-
-    const previousSongIndex = currentSongIndex - 1;
-    if (nextSongIndex <= album.songs.length) { return; }
-
-    const previousSong = album.songs[previousSongIndex];
-    player.playPause(previousSong);
-
+    const newSong = album.songs[prevSongIndex];
+    player.playPause(newSong);
   });
-}
+
+  $('button#next').click( function() {
+     if (player.playState !== 'playing') { return; }
+
+     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+     const nextSongIndex = currentSongIndex + 1;
+     if (nextSongIndex >= album.songs.length) { return; }
+
+     const nextSong = album.songs[nextSongIndex];
+     player.playPause(nextSong);
+   });
+});
